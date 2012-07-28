b52e7961ae34b85110f3f8bc6e603ebe52b80a7f
Carlos Zager <czager@zava.com.ve>
2012-07-27 21:21:03 -0430

    Cambios en modelos , vistas y administracion

--- pyconve/profiles/admin.py
+++ pyconve/profiles/admin.py
@@ -1,8 +1,11 @@
 #coding=utf-8
 from django.contrib import admin
-from profiles.models import Sponsor
+from profiles.models import *
 
 class SponsorAdmin(admin.ModelAdmin):
     fields = ('name', 'logo', 'website', 'sponsorship_type', 'description')
-
+    list_filter=('sponsorship_type',)
 admin.site.register(Sponsor, SponsorAdmin)
+
+admin.site.register(RegistrationProfile)
+admin.site.register(UserProfile)
