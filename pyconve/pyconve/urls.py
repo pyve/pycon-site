5e5cfba9f67baa62cc7fedb1cc455021fef91c9e
Israel Fermín Montilla <ifermin@zava.com.ve>
2012-10-09 15:13:27 -0430

    Agregada url para efectos de testing

--- pyconve/pyconve/urls.py
+++ pyconve/pyconve/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^cms/', include('cms.urls')),
     url(r'^profiles/', include('profiles.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'cms.views.home', name='home'),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'/home/israelord/Work/Zava/pycon/pyconve/media'}),
+    )
